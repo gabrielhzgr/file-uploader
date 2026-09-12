d9fd77fdd255b57f1bf1fd4735d2660cd346772c
gabrielhzgr <gabrielhzgr@gmail.com>
2026-09-12 17:07:28 -0600

    fix: 401 view was missing a title

--- controllers/authMiddleware.js
+++ controllers/authMiddleware.js
@@ -1,28 +1,26 @@
-const prisma = require("../lib/prisma")
+const prisma = require("../lib/prisma");
 
-function isAuthenticated (req,res,next){
-    if(!req.isAuthenticated()){
-        return res.status(401).render('401')
-    }
-    next()
+function isAuthenticated(req, res, next) {
+  if (!req.isAuthenticated()) {
+    return res.status(401).render("401", { title: "Unauthorized" });
+  }
+  next();
 }
 
-async function isOwner(req,res,next){
-    try {
-        let {user} = req
-        const {folderId} = req.params
-        
-        const folder = await prisma.folder.findFirst({where: {id: folderId}})        
-        if(folder !== null && folder.ownerId !== user.id){
-            return res.status(401).render('401')
-        }
-        req.folder = folder
-        next()
-    } catch (err) {
-        next(err)
+async function isOwner(req, res, next) {
+  try {
+    let { user } = req;
+    const { folderId } = req.params;
+
+    const folder = await prisma.folder.findFirst({ where: { id: folderId } });
+    if (folder !== null && folder.ownerId !== user.id) {
+      return res.status(401).render("401", { title: "Unauthorized" });
     }
-    
+    req.folder = folder;
+    next();
+  } catch (err) {
+    next(err);
+  }
 }
 
-
-module.exports = {isAuthenticated, isOwner}
+module.exports = { isAuthenticated, isOwner };
